53d587d6402c692e8b9dda55920038f3d206e5e9
Dylan Locksley <dylan011235@gmail.com>
2023-08-11 20:29:20 +0200

    Remove duplicate styles

--- src/components/BigButton.tsx
+++ src/components/BigButton.tsx
@@ -1,20 +1,25 @@
+import { useEffect } from "react";
 import { ButtonProps } from "../types/button";
 
 const BigButton = (props: ButtonProps) => {
     const {text, keyStyle, textStyle} = props
-    const bigButton = document.getElementById(text);
-    const style: string = 'w-1/2 m-2 p-2 hover:brightness-150 rounded  border-b-4'
 
-    if(bigButton) {
-        if(text === "RESET") {
-            bigButton.style.backgroundColor = keyStyle.specialKeyBg
-            bigButton.style.borderColor = keyStyle.specialKeyShw
-            bigButton.style.color = textStyle.text2
-        } else {
-            bigButton.style.backgroundColor = keyStyle.toggleEqBtn
-            bigButton.style.borderColor = keyStyle.toggleEqBtnShw
+    const style: string = 'w-1/2 m-2 p-2 hover:brightness-125 rounded  border-b-4'
+
+    useEffect(() => {
+        const bigButton = document.getElementById(text);
+
+        if(bigButton) {
+            if(text === "RESET") {
+                bigButton.style.backgroundColor = keyStyle.specialKeyBg
+                bigButton.style.borderColor = keyStyle.specialKeyShw
+                bigButton.style.color = textStyle.text2
+            } else {
+                bigButton.style.backgroundColor = keyStyle.toggleEqBtn
+                bigButton.style.borderColor = keyStyle.toggleEqBtnShw
+            }
         }
-    }
+    });
 
     return (
         <button className={style} id={text}>{text}</button>
