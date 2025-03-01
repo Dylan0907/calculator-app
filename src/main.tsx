add751e376b31edf8b006b96f818feb14919e431
Dylan Locksley <dylan011235@gmail.com>
2025-03-01 19:35:47 -0300

    Install Vite

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,11 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
+const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
